ce7edbbce7c3a9f62f9e0629e5f02a09d196aa59
vtu25754-srikar <vtu25754@veltech.edu.in>
2026-04-26 10:59:56 +0530

    Time: 1 ms (85.86%) | Memory: 45.6 MB (31.36%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(result, new ArrayList<>(), nums, used);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, boolean[] used) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            backtrack(result, temp, nums, used);
+
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
