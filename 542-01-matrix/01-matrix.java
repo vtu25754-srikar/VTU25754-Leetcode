e7365df4c89dcd3b0111b7a75bfcb64f5e581fb9
vtu25754-srikar <vtu25754@veltech.edu.in>
2026-04-26 10:10:26 +0530

    Time: 15 ms (42.23%) | Memory: 49.4 MB (33.29%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] dist = new int[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    queue.offer(new int[]{i, j});
+                } else {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int x = curr[0];
+            int y = curr[1];
+
+            for (int[] d : dirs) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                    if (dist[nx][ny] > dist[x][y] + 1) {
+                        dist[nx][ny] = dist[x][y] + 1;
+                        queue.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+}
